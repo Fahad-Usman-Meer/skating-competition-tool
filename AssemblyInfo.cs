f6eee89f38f5bb3dfd017e18e6de091f6d501ddb
fahad-meer <fahad.meer@northbaysolutions.net>
2022-06-04 02:18:37 +0500

    added compulsory resource in project

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("ClubCompFS")]
 [assembly: AssemblyFileVersion("1.0.0.6")]
 [assembly: Guid("3e46ffc1-e478-42fe-8ea7-69a013b492f9")]
-[assembly: AssemblyCopyright("Copyright ©  2018-2019")]
+[assembly: AssemblyCopyright("Copyright ©  2021-2022")]
 [assembly: AssemblyCompany("Susanne SW")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyTitle("ClubCompFS")]
