f6e6122b97b2850292b3d6e0721116438f7c333d
fahad-meer <fahad.meer@northbaysolutions.net>
2022-06-04 16:22:38 +0500

    Exclude Sequence Element (Sq) in excel

--- Module1.cs
+++ Module1.cs
@@ -3605,7 +3605,7 @@ namespace ClubCompFS
                             Module1.JDarr[checked(R + index + 8), 3] = "<<";
                         else if (str.Contains("<"))
                             Module1.JDarr[checked(R + index + 8), 3] = "<";
-                        else if (str.Contains("q"))
+                        else if (!str.Contains("Sq") && str.Contains("q")) // Exclude Sequence Element (Sq)
                             Module1.JDarr[checked(R + index + 8), 3] = "q";
                         int num5 = Microsoft.VisualBasic.Strings.InStr(1, str, " !", CompareMethod.Text);
                         if (num5 > 0)
